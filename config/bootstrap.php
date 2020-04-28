608040b3d1309f58c121fccb81f298feff157a81
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2020-04-28 13:51:15 +0200

    commit-navbar

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,7 +3,7 @@
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
-//Test
+
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php') && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
